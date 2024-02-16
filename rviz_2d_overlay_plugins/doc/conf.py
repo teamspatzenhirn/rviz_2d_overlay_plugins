2b0ed49b0dbd7b1096bb9eb6de63f7c2f4d1062f
Jonas Otto <jonas@jonasotto.com>
2024-02-16 15:57:44 +0100

    use less custom package docs with sphinx instead of doxygen

---
+++ rviz_2d_overlay_plugins/doc/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'rviz_2d_overlay_plugins'
+copyright = '2024, Domink Authaler, Jonas Otto, JSK Lab'
+author = 'Domink Authaler, Jonas Otto, JSK Lab'
+release = '1.3.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["myst_parser"]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+#html_static_path = ['_static']
